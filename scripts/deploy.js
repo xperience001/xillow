d95a043b4fb38b8205a3e4392fe0fa2189c570d4
xperience001 <tobilobatijani2011@gmail.com>
2023-01-20 22:09:14 +0100

    deploy-script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -62,6 +62,6 @@ async function main() {
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
+  console.error();
   process.exitCode = 1;
 });
