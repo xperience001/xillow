8f35c989e458ae76a67f74051051e95ba99ac05b
xperience001 <tobilobatijani2011@gmail.com>
2023-01-23 00:50:42 +0100

    fix-text

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 const Search = () => {
     return (
         <header>
-            <h2 className="header__title">Search it. Explore it. Buy it.</h2>
+            <h2 className="header__title">Search it. Explore it. Buy it</h2>
             <input
                 type="text"
                 className="header__search"
